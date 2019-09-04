3a8c4fdcd85e80b2a4cd46da412adce563995d21
Spencer6497 <peaces2@mymail.nku.edu>
2019-09-04 14:49:09 -0400

    Program is done, but logical errors abound

--- project1/project1.c
+++ project1/project1.c
@@ -27,30 +27,108 @@ void main() {
 	fp1 = fopen(filename1, "r");
 	fp2 = fopen(filename2, "r");
 
-	// Declare variables for user options
-	bool caseInsensitivity = true;
-	bool lettersOnly = true;
-
 	// Create char variables to store user responses and current characters from each file
 	char caseI;
 	char lettersOnlyVar;
 	char current;
 	char current2;
 
+	// Create int/float variables to store number of characters, mismatches, and non-letters (for testing letters only) respectively
+	int numCharacters = 0;
+	float numMismatches = 0.0;
+	float nonLetters = 0.0;
+
 	// Ask user for case-insensitivity or letters-only
 	printf("Case insensitive? (Y/N): ");
 	scanf(" %c", &caseI);
 
-	printf("Letters only? (Y/N): ");
+	printf("\nLetters only? (Y/N): ");
 	scanf(" %c", &lettersOnlyVar);
 	
-	// While loop that ensures EOF has not been met
-	
+	// Initialize both variables to the first letter of each file
 	current = getc(fp1);
-	 while(current != EOF) {
-		 printf("%c", current);
+	current2 = getc(fp2);
+
+	// While loop that ensures EOF has not been met
+	while(current != EOF) {
+		// Increment character count
+		numCharacters++;
+
+		// Conditional to test for case insensitivity and/or letters only
+		// Surely there is a better, cleaner way to do this, but I'm blanking. This is overtly redundant.
+
+		// If both case insensitive AND letters only
+		if ((caseI == 'y') && (lettersOnlyVar == 'y')) {
+			// Increment nonLetters if either character isn't alphabetic
+			if ((isalpha(current) == 0) || (isalpha(current2) == 0)) {
+				nonLetters++;
+			// Increment numMismatches if the characters don't equal each other
+			} else if (tolower(current) != tolower(current2)) {
+				numMismatches++;
+			}
+		}
+		// If case insensitive but not letters only
+		else if ((caseI == 'y') && (lettersOnlyVar == 'n')) {
+			// Increment numMismatches if characters don't equal
+			if (tolower(current) != tolower(current2)) {
+				numMismatches++;
+			}
+		}
+		// If case sensitive, but letters only
+		else if ((caseI == 'n') && (lettersOnlyVar == 'y')) {
+			// Increment nonLetters if either character isn't alphabetic
+			if ((isalpha(current) == 0) || (isalpha(current2) == 0)) {
+				nonLetters++;
+			}
+			else if (current != current2) {
+				numMismatches++;
+			}
+		}
+		// Default case, neither case insensitive nor letters only
+		else {
+			if (current != current2) {
+				numMismatches++;
+			}
+		}
 		current = getc(fp1);
+		current2 = getc(fp2);
 	 }
+
+	// Format mismatch percentage, non-tested percentage
+	float mismatch = (numMismatches / numCharacters) * 100;
+	float notTested = (nonLetters / numCharacters) * 100;
+
+	// Final print statements
+	printf("\nComparing %s to %s testing for ", filename1, filename2);
+
+
+	// If both case insensitive AND letters only
+	if ((caseI == 'y') && (lettersOnlyVar == 'y')) {
+		printf("case insensitivity and for letters only\n");
+		printf("\tFile sizes: %d characters", numCharacters);
+		printf("\n\tMismatches: %.2f%%", mismatch);
+		printf("\n\t Not tested: %.2f%%\n", notTested);
+	}
+	// If case insensitive but not letters only
+	else if ((caseI == 'y') && (lettersOnlyVar == 'n')) {
+		printf("case insensitivity\n");
+		printf("\tFile sizes: %d characters", numCharacters);
+		printf("\n\tMismatches: %.2f%%\n", mismatch);
+	}
+	// If case sensitive, but letters only
+	else if ((caseI == 'n') && (lettersOnlyVar == 'y')) {
+		printf("case sensitivity and for letters only\n");
+		printf("\tFile sizes: %d characters", numCharacters);
+		printf("\n\tMismatches: %.2f%%", mismatch);
+		printf("\n\tNot tested: %.2f%%\n", notTested);
+	}
+	// Default case, neither case insensitive nor letters only
+	else {
+		printf("case sensitivity and for all characters\n");
+		printf("\tFile sizes: %d characters", numCharacters);
+		printf("\n\tMismatches: %.2f%%\n", mismatch);
+	}
+
 	 fclose(fp1);
 	 fclose(fp2);
 	
